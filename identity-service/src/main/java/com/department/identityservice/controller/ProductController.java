5a66ed50ef13b944edfdb5154d19abdaed869852
tungnguyenthanh888 <tungdev3010@gmail.com>
2026-05-21 17:52:38 +0700

    update

---
+++ identity-service/src/main/java/com/department/identityservice/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.department.identityservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @GetMapping
+    public String demo()
+    {
+        return "Hello";
+    }
+}
